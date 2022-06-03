4c5f1c300b0dd35f95235dedb77559929c6ffe76
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2022-06-02 20:47:17 -0500

    form4-complete

--- frontend/src/app/components/stepper-forms/form03/form03.component.ts
+++ frontend/src/app/components/stepper-forms/form03/form03.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+
+declare var jQuery:any;
+declare var $:any;
 
 @Component({
   selector: 'app-form03',
   templateUrl: './form03.component.html',
   styleUrls: ['./form03.component.css']
 })
-export class Form03Component implements OnInit {
+export class Form03Component implements OnInit, DoCheck {
+  petrol: boolean;
+  petrol_design: boolean;
+  diesel: boolean;
+  electric: boolean;
+  hybrid: boolean;
+  scooters: boolean;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.petrol = false;
+    this.petrol_design = false;
+    this.diesel = false;
+    this.electric = false;
+    this.hybrid = false;
+    this.scooters = false;
+
+   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: any){
+
+  }
+
+  status(type: string) {
+    if(type == 'petrol') {
+      this.petrol = !(this.petrol);
+    } else if(type == 'diesel') {
+      this.diesel = !(this.diesel);
+    } else if(type == 'electric') {
+      this.electric = !(this.electric);
+    } else {
+      this.hybrid = !(this.hybrid);
+    }
+  }
+
+  update_style (type: any, type_str: string) {
+    if (type) {
+
+      $(type_str).removeClass('test');
+      $(type_str).addClass('test-ts');
+    } else {
+      $(type_str).removeClass('test-ts');
+      $(type_str).addClass('test');
+    }
+  }
+
+  ngDoCheck() {
+    this.update_style(this.petrol, '#petrol');
+    this.update_style(this.diesel, '#diesel');
+    this.update_style(this.electric, '#electric');
+    this.update_style(this.hybrid, '#hybrid');
   }
 
 }
--- frontend/src/app/components/stepper-forms/form04/form04.component.ts
+++ frontend/src/app/components/stepper-forms/form04/form04.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+
+declare var jQuery:any;
+declare var $:any;
 
 @Component({
   selector: 'app-form04',
   templateUrl: './form04.component.html',
   styleUrls: ['./form04.component.css']
 })
-export class Form04Component implements OnInit {
 
-  constructor() { }
+export class Form04Component implements OnInit, DoCheck {
+  public properties: boolean[];
+  public properties_name: string[];
+
+  constructor() {
+    this.properties = Array();
+    this.properties_name = Array(
+      'antitheft_alarm',
+      'usb_port',
+      'digital_board',
+      'analog_board',
+      'grill_with_backrest',
+      'load_grill',
+      'italian_exhaust',
+      'dual_shotgun_exhaust',
+      'low_center_of_gravity_exhaust',
+      'brake_type',
+      'trunk',
+      'type_of_rims',
+      'headlight_type',
+      'tires',
+      'inverted_front_suspension',
+      'monoshock_suspension'
+    );
+
+    this.properties_name.forEach(element =>
+      this.properties.push(false)
+    );
+
+  }
 
   ngOnInit(): void {
+    //console.log(this.properties);
+  }
+
+  onSubmit(form: any) {
+
+  }
+
+  status(index: any){
+    this.properties[index] = !(this.properties[index]);
+  }
+
+  update_style (type: any, type_str: string) {
+    if (type) {
+      $(type_str).removeClass('check-false');
+      $(type_str).addClass('check-true');
+    } else {
+      $(type_str).removeClass('check-true');
+      $(type_str).addClass('check-false');
+    }
+  }
+
+  ngDoCheck(): void {
+    for (let index = 0; index < this.properties.length; index++) {
+      this.update_style(this.properties[index], "#"+this.properties_name[index]);
+    }
   }
 
 }
