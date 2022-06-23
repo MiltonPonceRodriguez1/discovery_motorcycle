91eef9d945237e460a9f03a652f107b3ce6ab93d
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2022-06-22 21:35:02 -0500

    form5-complete

--- frontend/src/app/components/stepper-forms/form05/form05.component.ts
+++ frontend/src/app/components/stepper-forms/form05/form05.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-form05',
   templateUrl: './form05.component.html',
   styleUrls: ['./form05.component.css']
 })
-export class Form05Component implements OnInit {
 
-  constructor() { }
+export class Form05Component implements OnInit, DoCheck {
+
+  fuel_tank_threshold_max :number = 4;
+  fuel_tank_threshold_min :number = 24;
+  fuel_tank_min: number = 4;
+  fuel_tank_max: number = 25;
+
+  /* INICIO CONIGURACIÓN DEL SLIDER */
+  autoTicks = false;
+  disabled = false;
+  invert = false;
+  showTicks = false;
+  step = 1;
+  thumbLabel = true;
+  value = 0;
+  vertical = false;
+  tickInterval = 1;
+  max_slider_fuel_tank = 25;
+  min_slider_fuel_tank = 4;
+  /* FIN CONIGURACIÓN DEL SLIDER */
+
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: any) {
+
+  }
+
+  getSliderTickInterval(): number | 'auto' {
+    if (this.showTicks) {
+      return this.autoTicks ? 'auto' : this.tickInterval;
+    }
+    return 0;
+  }
+
+  ngDoCheck(): void {
+    this.fuel_tank_threshold_max = this.fuel_tank_min + 1;
+    this.fuel_tank_threshold_min = this.fuel_tank_max -1;
   }
 
 }
